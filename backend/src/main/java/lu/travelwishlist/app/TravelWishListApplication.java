a1868b8ee2b08a5493fee7e4e02e37391d43401e
navidvafaei2020 <navidvafaei2020@gmail.com>
2026-01-17 07:54:55 +0100

    Define application.properties file and refining Main class

---
+++ backend/src/main/java/lu/travelwishlist/app/TravelWishListApplication.java
@@ -0,0 +1,15 @@
+package lu.travelwishlist.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TravelWishListApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TravelWishListApplication.class, args);
+    }
+
+}
